7077bf503f85e1a2cb703d2661041d18ae3f6bb9
Matheus Bozetti <matheusbzt@gmail.com>
2025-02-13 18:27:09 -0400

    ✨ add time-historic icon

--- packages/vue-icons/src/types/IconSize.ts
+++ packages/vue-icons/src/types/IconSize.ts
@@ -1,2 +1,2 @@
-export const IconSizeOptions = ['is-small', 'is-medium', 'is-large', 'is-default', 'is-12', 'is-14', 'is-26', 'is-28', 'is-36'] as const;
+export const IconSizeOptions = ['is-small', 'is-medium', 'is-large', 'is-default', 'is-12', 'is-14', 'is-16', 'is-26', 'is-28', 'is-36'] as const;
 export type IconsSize = (typeof IconSizeOptions)[number];
